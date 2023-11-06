dfe0212a048f3655dcd3b96621b3a9ad029ab324
TriBrid <shigesaku.390@gmail.com>
2023-11-07 02:25:19 +0900

    Stage_1

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class GameMaster : MonoBehaviour{
+
+	public Transform counter_trans;
+	
+	private int counter = 0;
+	private int stage_index = 0;
+
+	void Start(){
+		
+	}
+
+	public void count_increment(){
+		counter++;
+		if(counter == 24){
+			stage_index++;
+			counter = 0;
+		}
+
+		counter_trans.GetComponent<Text>().text = counter.ToString();
+	}
+
+	public int get_stage_index(){
+		return stage_index;
+	}
+}
