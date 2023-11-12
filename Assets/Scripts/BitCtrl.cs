27e55c8486a13ecbc9aa87523526e35fdf3e185b
TriBrid <shigesaku.390@gmail.com>
2023-11-13 05:32:09 +0900

    change stage admin system

--- Assets/Scripts/BitCtrl.cs
+++ Assets/Scripts/BitCtrl.cs
@@ -14,15 +14,15 @@ public class BitCtrl : MonoBehaviour, IDragHandler, IPointerClickHandler{
 	private static List<bool> binaries = new List<bool>(){false, false, false, false, false, false, false, false};
 
 	public void OnDrag(PointerEventData eventData){
-		if(master.get_stage_index() == 1){
+		if(!master.get_clear_flag(1)){
 			float axis_y = eventData.position.y-700f;
 			transform.localPosition = new Vector3(transform.localPosition.x, axis_y);
-			master.count_set(Mathf.FloorToInt(axis_y / 100f)+22);
+			master.count_set(Mathf.FloorToInt(axis_y / 100f)+22, 1);
 		}
 	}
 
 	public void OnPointerClick(PointerEventData eventData){
-		if(master.get_stage_index() == 2){
+		if(!master.get_clear_flag(2)){
 			if(binaries[bit_number]){
 				transform.DOLocalMoveY(transform.localPosition.y-50f, 0.5f);
 			}else{
@@ -36,9 +36,8 @@ public class BitCtrl : MonoBehaviour, IDragHandler, IPointerClickHandler{
 					result += (int)Mathf.Pow(2, 7-i);
 				}
 			}
-			master.count_set(result);
+			master.count_set(result, 2);
 		}
-
 	}
 
 	void Start(){
--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using System.Threading;
 
 public class GameMaster : MonoBehaviour{
 
@@ -10,49 +11,50 @@ public class GameMaster : MonoBehaviour{
 	public InputExt inputext_ins;
 
 	private int counter = 0;
-	private int stage_index = 0;
+	private int stage_max = 15;
+
+	private List<bool> clear_flags = new List<bool>();
 
 	void Start(){
 		initial_placement();
+
+		for(int i=0; i<stage_max; i++) clear_flags.Add(false);
 	}
 
 	public void initial_placement(){
 		bits_trans.localPosition = new Vector2(0, -650f);
 	}
 
-	public void count_increment(){
+	public void count_increment(int gimmickID){
 		counter++;
 		counter_trans.GetComponent<Text>().text = counter.ToString();
 
-		if(counter == 24) next_stage();
+		if(counter == 24) clear_stage(gimmickID);
 	}
 
-	public void count_set(int number){
+	public void count_set(int number, int gimmickID){
 		counter = number;
 		counter_trans.GetComponent<Text>().text = counter.ToString();
 
-		if(counter == 24) next_stage();
+		if(counter == 24) clear_stage(gimmickID);
 	}
 
-	public void next_stage(){
-		stage_index++;
-		
-		// I want to show "24" before stage advance.
+	public void clear_stage(int stage){
 		DOVirtual.DelayedCall(1.0f, ()=>{
-			counter = 0;
-			counter_trans.GetComponent<Text>().text = counter.ToString();
+			clear_flags[stage] = true;
+			count_set(0, 0);
 
-			if(stage_index == 1){
+			if(stage == 0){
 				bits_trans.DOLocalMoveY(-600f, 1.0f);
 			}
-			if(stage_index == 2){
+			if(stage == 1){
 				bits_trans.DOLocalMoveY(-550f, 1.0f);
 				bits_trans.GetChild(0).DOLocalMoveY(-700f, 0.5f);
 			}
-			if(stage_index == 3){
-				clicktostart_trans.GetComponent<StartBtnUnit>().Stage4_preparation();
+			if(stage == 2){
+				clicktostart_trans.GetComponent<StartBtnUnit>().Stage3_preparation();
 			}
-			if(stage_index == 4){
+			if(stage == 3){
 				inputext_ins.isEnable = true;
 				inputext_ins.Add_target(new List<string>(){"2", "4", "t", "w", "e", "n", "y", "space", "f", "o", "u", "r"});
 				inputext_ins.SetInputCallback(()=>{
@@ -66,7 +68,8 @@ public class GameMaster : MonoBehaviour{
 
 					// judge
 					if(inputext_ins.GetStream() == "24"){
-						next_stage();
+						// next_stage();
+						clear_stage(4);
 						inputext_ins.isEnable = false;
 						DOVirtual.DelayedCall(1.0f, ()=>{
 							inputext_ins.ClearStream();
@@ -74,7 +77,7 @@ public class GameMaster : MonoBehaviour{
 						});
 					}
 					if(inputext_ins.GetStream() == "twenty four"){
-						next_stage();
+						clear_stage(5);
 						inputext_ins.isEnable = false;
 						DOVirtual.DelayedCall(1.0f, ()=>{
 							inputext_ins.ClearStream();
@@ -86,7 +89,7 @@ public class GameMaster : MonoBehaviour{
 		});
 	}
 
-	public int get_stage_index(){
-		return stage_index;
+	public bool get_clear_flag(int stage_idx){
+		return clear_flags[stage_idx];
 	}
 }
